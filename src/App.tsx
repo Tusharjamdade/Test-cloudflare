ce303b64722b9c51ee38d43d864ac3c5480cc570
Tusharjamdade <tusharnjamdade@gmail.com>
2024-07-15 22:18:37 +0530

    Creating TaskBidder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Herosection from "../Component/Herosection";
+import Navbar from "../Component/Navbar";
+import LoginPage from "../Component/LoginPage";
+import SignUp from "../Component/SignUp";
+import UserProfile from "../Component/UserProfile";
+import CreateProfile from "../Component/CreateProfile";
+import Footer from "../Component/Footer";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-     hehehe boi
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Herosection />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/createprofile" element={<CreateProfile />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
