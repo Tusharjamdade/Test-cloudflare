cde5bb923e88bad992f936d646e9a87b50cf9322
Tusharjamdade <tusharnjamdade@gmail.com>
2024-07-16 21:34:23 +0530

    Added HamBurger

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import CreateProfile from "../Component/CreateProfile";
 import Footer from "../Component/Footer";
 
 function App() {
-  // const [count, setCount] = useState(0)
+ 
 
   return (
     <>
